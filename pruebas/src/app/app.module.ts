64745041aa007af432d9f035eeff34a241732d8a
Daniel Lopez <ext_delopezp@falabella.cl>
2020-06-11 19:34:14 -0400

    Configuraciones de las pruebas de integración

--- pruebas/src/app/app.module.ts
+++ pruebas/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MedicosComponent } from './basicas/intermedio/espias/medicos.component';
+import { MedicosComponent } from './intermedio/espias/medicos.component';
+import { MedicoComponent } from './intermedio2/medico/medico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MedicosComponent
+    MedicosComponent,
+    MedicoComponent
   ],
   imports: [
     BrowserModule,
---
+++ pruebas/src/app/intermedio2/medico/medico.component.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { MedicoComponent } from './medico.component';
+
+describe('Medico Component', () => {
+  let component: MedicoComponent;
+  let fixture: ComponentFixture<MedicoComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MedicoComponent]
+    });
+
+    fixture = TestBed.createComponent(MedicoComponent);
+    component = fixture.componentInstance;
+  });
+});
